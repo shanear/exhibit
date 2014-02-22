56255db5a24304a38e1aedd7f5748355352c2c05
Shane Russell <shane1337@gmail.com>
2014-02-21 17:50:30 -0800

    Basic beacon functionality

--- Exhibit/EXBViewController.m
+++ Exhibit/EXBViewController.m
@@ -6,10 +6,13 @@
 //  Copyright (c) 2014 Happy Hour Devs. All rights reserved.
 //
 
+#import <ESTBeaconManager.h>
+
 #import "EXBViewController.h"
 
-@interface EXBViewController ()
-
+@interface EXBViewController () <ESTBeaconManagerDelegate>
+@property (nonatomic, strong) ESTBeaconManager* beaconManager;
+@property (nonatomic, strong) ESTBeaconRegion *beaconRegion;
 @end
 
 @implementation EXBViewController
@@ -17,7 +20,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.beaconManager = [[ESTBeaconManager alloc] init];
+    self.beaconManager.delegate = self;
+    self.beaconManager.avoidUnknownStateBeacons = YES;
+    
+    self.beaconRegion = [[ESTBeaconRegion alloc] initWithProximityUUID: ESTIMOTE_PROXIMITY_UUID
+                                                            identifier: @"EstimoteSampleRegion"];
+    self.beaconRegion.notifyEntryStateOnDisplay = true;
+    
+    [self.beaconManager startRangingBeaconsInRegion:self.beaconRegion];
+}
+
+-(void)beaconManager:(ESTBeaconManager *)manager
+     didRangeBeacons:(NSArray *)beacons
+            inRegion:(ESTBeaconRegion *)region
+{
+    NSLog([NSString stringWithFormat:@"We're looking at %d beacons", [beacons count]]);
 }
 
 - (void)didReceiveMemoryWarning
