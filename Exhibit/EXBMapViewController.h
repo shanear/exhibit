6c9ba8ea6eb378900c7e589ff7e4222dfc67c22e
Andrew Finch <andrew.a.finch@gmail.com>
2014-02-21 21:12:01 -0800

    Adds image assets, EXBMapViewController

---
+++ Exhibit/EXBMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  EXBMapViewController.h
+//  Exhibit
+//
+//  Created by Andrew Finch on 2/21/14.
+//  Copyright (c) 2014 Happy Hour Devs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EXBMapViewController : UIViewController
+
+@end
---
+++ Exhibit/EXBMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  EXBMapViewController.m
+//  Exhibit
+//
+//  Created by Andrew Finch on 2/21/14.
+//  Copyright (c) 2014 Happy Hour Devs. All rights reserved.
+//
+
+#import "EXBMapViewController.h"
+
+@interface EXBMapViewController ()
+
+@end
+
+@implementation EXBMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
